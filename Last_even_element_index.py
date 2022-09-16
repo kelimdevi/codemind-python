0bd18c858c22d09b76dc88b9559aa33fde4f0429
kelimdevi <84917318+kelimdevi@users.noreply.github.com>
2022-09-16 21:59:18 +0530

    n=int(input()) a=list(map(int,input().split())) lasteven=0 for i in range(n):     if a[i]%2==0:         lasteven=i print(lasteven)

--- Last_even_element_index.py
+++ Last_even_element_index.py
@@ -1,7 +1,7 @@
 n=int(input())
-b=list(map(int,input().split()))
+a=list(map(int,input().split()))
 lasteven=0
 for i in range(n):
-    if b[i]%2==0:
+    if a[i]%2==0:
         lasteven=i
 print(lasteven)
